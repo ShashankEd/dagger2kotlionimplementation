c8b65e698c29e5ba9236dad513c1d3ef950005c3
Shashank Gupta <shashankgupta@TSM-SHASHANK.local>
2020-12-30 22:02:08 +0530

    Fueled injection

--- app/src/main/java/com/coder/daggerdemoapp/MainActivity.kt
+++ app/src/main/java/com/coder/daggerdemoapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.coder.daggerdemoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.coder.daggerdemoapp.classes.Car
 import com.coder.daggerdemoapp.classes.CarComponent
 import com.coder.daggerdemoapp.classes.DaggerCarComponent
@@ -13,7 +14,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        crateCar();
+//        crateCar();
+        //fueled injection
+        createCarWithFueledInjection()
     }
     private fun crateCar() {
         //get the dependencies done through dagger component for the
@@ -24,7 +27,19 @@ class MainActivity : AppCompatActivity() {
         car = obj.getCar();
         //call the drive method, we must notice that we haven't instantiated the Car
         //or wheel or engine, the dagger component has taken care of that
+        Log.d("constructor injection ", "Using normal constructor injection: ");
         car.drive();
+    }
 
+    //fueled injection
+    private fun createCarWithFueledInjection() {
+        //get the dependencies done through dagger component for the
+        //car component interface
+        var  obj:CarComponent = DaggerCarComponent.builder()
+            .build();
+        //injecting the main activity, which will create the car using car component and its dependencies
+        obj.inject(this);
+        Log.d("Fueled injection ", "Using fueled injection: ");
+        car.drive();
     }
 }
--- app/src/main/java/com/coder/daggerdemoapp/classes /CarComponent.kt
+++ app/src/main/java/com/coder/daggerdemoapp/classes /CarComponent.kt
@@ -1,8 +1,11 @@
 package com.coder.daggerdemoapp.classes
 
+import com.coder.daggerdemoapp.MainActivity
 import dagger.Component
 
 @Component
 interface CarComponent {
     fun getCar() : Car
+    //for fueled injection
+    fun inject(mainActivity: MainActivity);
 }
